30e31a02b9908bdb1724430407f4058a91d2061a
Joe Rugezo <joedassin.rugezo@stu.ocsb.ca>
2023-11-21 17:52:04 +0000

    Modified factorial.py

--- factorial.py
+++ factorial.py
@@ -4,20 +4,27 @@ def main():
     factorial_answer = 1
 
     # get the user number
-    user_number = int(input("Enter a positive number: "))
+    user_number = input("Enter a positive number: ")
     print("")
+    try:
+        user_number =int(user_number)
+    except ValueError:
+        print("invalid input")
+    else:
+        if user_number >= 0:
+            # replicates a do..while loop
+            # calculate the factorial of the user number using a loop
+            while True:
+                loop_counter = loop_counter + 1
+                factorial_answer = factorial_answer * loop_counter
+                print("Tracking {} times through loop.".format(loop_counter))
+                if loop_counter >= user_number:
+                    break
 
-    # replicates a do..while loop
-    # calculate the factorial of the user number using a loop
-    while True:
-        loop_counter = loop_counter + 1
-        factorial_answer = factorial_answer * loop_counter
-        print("Tracking {} times through loop.".format(loop_counter))
-        if loop_counter >= user_number:
-            break
-
-    print("")
-    print("{}!= {}".format(user_number, factorial_answer))
+            print("")
+            print("{}!= {}".format(user_number, factorial_answer))
+        else:
+            print("Please enter valid input")
 
 
 if __name__ == "__main__":
